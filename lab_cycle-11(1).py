39601ce163ca559d57de9207b89b4745745405d4
VivekaMurali <77691652+VivekaMurali@users.noreply.github.com>
2021-02-01 20:37:55 +0530

    Add files via upload
    Work with built-in modules – calendar, datetime

---
+++ lab_cycle-11(1).py
@@ -0,0 +1,10 @@
+import calendar
+from datetime import *
+print(calendar.month(2021,1))
+d=datetime.today()
+print('Year=',d.year)
+print('Month=',d.month)
+print('Hour=',d.hour)
+print('Minute=',d.minute)
+print('Second=',d.second)
+print('Date and time=',d)
